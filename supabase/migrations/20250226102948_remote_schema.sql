3a6c5e980a737262b3fc195762c22379375effdf
Bruno-Ajhenberger <bruc253@gmail.com>
2025-02-26 11:40:45 +0100

    remote migration

---
+++ supabase/migrations/20250226102948_remote_schema.sql
@@ -0,0 +1,582 @@
+revoke delete on table "public"."event_groups" from "anon";
+
+revoke insert on table "public"."event_groups" from "anon";
+
+revoke references on table "public"."event_groups" from "anon";
+
+revoke select on table "public"."event_groups" from "anon";
+
+revoke trigger on table "public"."event_groups" from "anon";
+
+revoke truncate on table "public"."event_groups" from "anon";
+
+revoke update on table "public"."event_groups" from "anon";
+
+revoke delete on table "public"."event_groups" from "authenticated";
+
+revoke insert on table "public"."event_groups" from "authenticated";
+
+revoke references on table "public"."event_groups" from "authenticated";
+
+revoke select on table "public"."event_groups" from "authenticated";
+
+revoke trigger on table "public"."event_groups" from "authenticated";
+
+revoke truncate on table "public"."event_groups" from "authenticated";
+
+revoke update on table "public"."event_groups" from "authenticated";
+
+revoke delete on table "public"."event_groups" from "service_role";
+
+revoke insert on table "public"."event_groups" from "service_role";
+
+revoke references on table "public"."event_groups" from "service_role";
+
+revoke select on table "public"."event_groups" from "service_role";
+
+revoke trigger on table "public"."event_groups" from "service_role";
+
+revoke truncate on table "public"."event_groups" from "service_role";
+
+revoke update on table "public"."event_groups" from "service_role";
+
+revoke delete on table "public"."event_types" from "anon";
+
+revoke insert on table "public"."event_types" from "anon";
+
+revoke references on table "public"."event_types" from "anon";
+
+revoke select on table "public"."event_types" from "anon";
+
+revoke trigger on table "public"."event_types" from "anon";
+
+revoke truncate on table "public"."event_types" from "anon";
+
+revoke update on table "public"."event_types" from "anon";
+
+revoke delete on table "public"."event_types" from "authenticated";
+
+revoke insert on table "public"."event_types" from "authenticated";
+
+revoke references on table "public"."event_types" from "authenticated";
+
+revoke select on table "public"."event_types" from "authenticated";
+
+revoke trigger on table "public"."event_types" from "authenticated";
+
+revoke truncate on table "public"."event_types" from "authenticated";
+
+revoke update on table "public"."event_types" from "authenticated";
+
+revoke delete on table "public"."event_types" from "service_role";
+
+revoke insert on table "public"."event_types" from "service_role";
+
+revoke references on table "public"."event_types" from "service_role";
+
+revoke select on table "public"."event_types" from "service_role";
+
+revoke trigger on table "public"."event_types" from "service_role";
+
+revoke truncate on table "public"."event_types" from "service_role";
+
+revoke update on table "public"."event_types" from "service_role";
+
+revoke delete on table "public"."events" from "anon";
+
+revoke insert on table "public"."events" from "anon";
+
+revoke references on table "public"."events" from "anon";
+
+revoke select on table "public"."events" from "anon";
+
+revoke trigger on table "public"."events" from "anon";
+
+revoke truncate on table "public"."events" from "anon";
+
+revoke update on table "public"."events" from "anon";
+
+revoke delete on table "public"."events" from "authenticated";
+
+revoke insert on table "public"."events" from "authenticated";
+
+revoke references on table "public"."events" from "authenticated";
+
+revoke select on table "public"."events" from "authenticated";
+
+revoke trigger on table "public"."events" from "authenticated";
+
+revoke truncate on table "public"."events" from "authenticated";
+
+revoke update on table "public"."events" from "authenticated";
+
+revoke delete on table "public"."events" from "service_role";
+
+revoke insert on table "public"."events" from "service_role";
+
+revoke references on table "public"."events" from "service_role";
+
+revoke select on table "public"."events" from "service_role";
+
+revoke trigger on table "public"."events" from "service_role";
+
+revoke truncate on table "public"."events" from "service_role";
+
+revoke update on table "public"."events" from "service_role";
+
+revoke delete on table "public"."user_roles" from "anon";
+
+revoke insert on table "public"."user_roles" from "anon";
+
+revoke references on table "public"."user_roles" from "anon";
+
+revoke select on table "public"."user_roles" from "anon";
+
+revoke trigger on table "public"."user_roles" from "anon";
+
+revoke truncate on table "public"."user_roles" from "anon";
+
+revoke update on table "public"."user_roles" from "anon";
+
+revoke delete on table "public"."user_roles" from "authenticated";
+
+revoke insert on table "public"."user_roles" from "authenticated";
+
+revoke references on table "public"."user_roles" from "authenticated";
+
+revoke select on table "public"."user_roles" from "authenticated";
+
+revoke trigger on table "public"."user_roles" from "authenticated";
+
+revoke truncate on table "public"."user_roles" from "authenticated";
+
+revoke update on table "public"."user_roles" from "authenticated";
+
+revoke delete on table "public"."user_roles" from "service_role";
+
+revoke insert on table "public"."user_roles" from "service_role";
+
+revoke references on table "public"."user_roles" from "service_role";
+
+revoke select on table "public"."user_roles" from "service_role";
+
+revoke trigger on table "public"."user_roles" from "service_role";
+
+revoke truncate on table "public"."user_roles" from "service_role";
+
+revoke update on table "public"."user_roles" from "service_role";
+
+revoke delete on table "public"."users" from "anon";
+
+revoke insert on table "public"."users" from "anon";
+
+revoke references on table "public"."users" from "anon";
+
+revoke select on table "public"."users" from "anon";
+
+revoke trigger on table "public"."users" from "anon";
+
+revoke truncate on table "public"."users" from "anon";
+
+revoke update on table "public"."users" from "anon";
+
+revoke delete on table "public"."users" from "authenticated";
+
+revoke insert on table "public"."users" from "authenticated";
+
+revoke references on table "public"."users" from "authenticated";
+
+revoke select on table "public"."users" from "authenticated";
+
+revoke trigger on table "public"."users" from "authenticated";
+
+revoke truncate on table "public"."users" from "authenticated";
+
+revoke update on table "public"."users" from "authenticated";
+
+revoke delete on table "public"."users" from "service_role";
+
+revoke insert on table "public"."users" from "service_role";
+
+revoke references on table "public"."users" from "service_role";
+
+revoke select on table "public"."users" from "service_role";
+
+revoke trigger on table "public"."users" from "service_role";
+
+revoke truncate on table "public"."users" from "service_role";
+
+revoke update on table "public"."users" from "service_role";
+
+revoke delete on table "public"."venue_status_states" from "anon";
+
+revoke insert on table "public"."venue_status_states" from "anon";
+
+revoke references on table "public"."venue_status_states" from "anon";
+
+revoke select on table "public"."venue_status_states" from "anon";
+
+revoke trigger on table "public"."venue_status_states" from "anon";
+
+revoke truncate on table "public"."venue_status_states" from "anon";
+
+revoke update on table "public"."venue_status_states" from "anon";
+
+revoke delete on table "public"."venue_status_states" from "authenticated";
+
+revoke insert on table "public"."venue_status_states" from "authenticated";
+
+revoke references on table "public"."venue_status_states" from "authenticated";
+
+revoke select on table "public"."venue_status_states" from "authenticated";
+
+revoke trigger on table "public"."venue_status_states" from "authenticated";
+
+revoke truncate on table "public"."venue_status_states" from "authenticated";
+
+revoke update on table "public"."venue_status_states" from "authenticated";
+
+revoke delete on table "public"."venue_status_states" from "service_role";
+
+revoke insert on table "public"."venue_status_states" from "service_role";
+
+revoke references on table "public"."venue_status_states" from "service_role";
+
+revoke select on table "public"."venue_status_states" from "service_role";
+
+revoke trigger on table "public"."venue_status_states" from "service_role";
+
+revoke truncate on table "public"."venue_status_states" from "service_role";
+
+revoke update on table "public"."venue_status_states" from "service_role";
+
+revoke delete on table "public"."venue_status_types" from "anon";
+
+revoke insert on table "public"."venue_status_types" from "anon";
+
+revoke references on table "public"."venue_status_types" from "anon";
+
+revoke select on table "public"."venue_status_types" from "anon";
+
+revoke trigger on table "public"."venue_status_types" from "anon";
+
+revoke truncate on table "public"."venue_status_types" from "anon";
+
+revoke update on table "public"."venue_status_types" from "anon";
+
+revoke delete on table "public"."venue_status_types" from "authenticated";
+
+revoke insert on table "public"."venue_status_types" from "authenticated";
+
+revoke references on table "public"."venue_status_types" from "authenticated";
+
+revoke select on table "public"."venue_status_types" from "authenticated";
+
+revoke trigger on table "public"."venue_status_types" from "authenticated";
+
+revoke truncate on table "public"."venue_status_types" from "authenticated";
+
+revoke update on table "public"."venue_status_types" from "authenticated";
+
+revoke delete on table "public"."venue_status_types" from "service_role";
+
+revoke insert on table "public"."venue_status_types" from "service_role";
+
+revoke references on table "public"."venue_status_types" from "service_role";
+
+revoke select on table "public"."venue_status_types" from "service_role";
+
+revoke trigger on table "public"."venue_status_types" from "service_role";
+
+revoke truncate on table "public"."venue_status_types" from "service_role";
+
+revoke update on table "public"."venue_status_types" from "service_role";
+
+revoke delete on table "public"."venue_statuses" from "anon";
+
+revoke insert on table "public"."venue_statuses" from "anon";
+
+revoke references on table "public"."venue_statuses" from "anon";
+
+revoke select on table "public"."venue_statuses" from "anon";
+
+revoke trigger on table "public"."venue_statuses" from "anon";
+
+revoke truncate on table "public"."venue_statuses" from "anon";
+
+revoke update on table "public"."venue_statuses" from "anon";
+
+revoke delete on table "public"."venue_statuses" from "authenticated";
+
+revoke insert on table "public"."venue_statuses" from "authenticated";
+
+revoke references on table "public"."venue_statuses" from "authenticated";
+
+revoke select on table "public"."venue_statuses" from "authenticated";
+
+revoke trigger on table "public"."venue_statuses" from "authenticated";
+
+revoke truncate on table "public"."venue_statuses" from "authenticated";
+
+revoke update on table "public"."venue_statuses" from "authenticated";
+
+revoke delete on table "public"."venue_statuses" from "service_role";
+
+revoke insert on table "public"."venue_statuses" from "service_role";
+
+revoke references on table "public"."venue_statuses" from "service_role";
+
+revoke select on table "public"."venue_statuses" from "service_role";
+
+revoke trigger on table "public"."venue_statuses" from "service_role";
+
+revoke truncate on table "public"."venue_statuses" from "service_role";
+
+revoke update on table "public"."venue_statuses" from "service_role";
+
+revoke delete on table "public"."venue_types" from "anon";
+
+revoke insert on table "public"."venue_types" from "anon";
+
+revoke references on table "public"."venue_types" from "anon";
+
+revoke select on table "public"."venue_types" from "anon";
+
+revoke trigger on table "public"."venue_types" from "anon";
+
+revoke truncate on table "public"."venue_types" from "anon";
+
+revoke update on table "public"."venue_types" from "anon";
+
+revoke delete on table "public"."venue_types" from "authenticated";
+
+revoke insert on table "public"."venue_types" from "authenticated";
+
+revoke references on table "public"."venue_types" from "authenticated";
+
+revoke select on table "public"."venue_types" from "authenticated";
+
+revoke trigger on table "public"."venue_types" from "authenticated";
+
+revoke truncate on table "public"."venue_types" from "authenticated";
+
+revoke update on table "public"."venue_types" from "authenticated";
+
+revoke delete on table "public"."venue_types" from "service_role";
+
+revoke insert on table "public"."venue_types" from "service_role";
+
+revoke references on table "public"."venue_types" from "service_role";
+
+revoke select on table "public"."venue_types" from "service_role";
+
+revoke trigger on table "public"."venue_types" from "service_role";
+
+revoke truncate on table "public"."venue_types" from "service_role";
+
+revoke update on table "public"."venue_types" from "service_role";
+
+revoke delete on table "public"."venues" from "anon";
+
+revoke insert on table "public"."venues" from "anon";
+
+revoke references on table "public"."venues" from "anon";
+
+revoke select on table "public"."venues" from "anon";
+
+revoke trigger on table "public"."venues" from "anon";
+
+revoke truncate on table "public"."venues" from "anon";
+
+revoke update on table "public"."venues" from "anon";
+
+revoke delete on table "public"."venues" from "authenticated";
+
+revoke insert on table "public"."venues" from "authenticated";
+
+revoke references on table "public"."venues" from "authenticated";
+
+revoke select on table "public"."venues" from "authenticated";
+
+revoke trigger on table "public"."venues" from "authenticated";
+
+revoke truncate on table "public"."venues" from "authenticated";
+
+revoke update on table "public"."venues" from "authenticated";
+
+revoke delete on table "public"."venues" from "service_role";
+
+revoke insert on table "public"."venues" from "service_role";
+
+revoke references on table "public"."venues" from "service_role";
+
+revoke select on table "public"."venues" from "service_role";
+
+revoke trigger on table "public"."venues" from "service_role";
+
+revoke truncate on table "public"."venues" from "service_role";
+
+revoke update on table "public"."venues" from "service_role";
+
+alter table "public"."event_groups" drop constraint "event_groups_event_id_fkey";
+
+alter table "public"."event_groups" drop constraint "event_groups_event_id_user_id_key";
+
+alter table "public"."event_groups" drop constraint "event_groups_user_id_fkey";
+
+alter table "public"."events" drop constraint "events_venue_status_id_fkey";
+
+alter table "public"."users" drop constraint "users_user_id_fkey";
+
+alter table "public"."users" drop constraint "users_user_role_id_fkey";
+
+alter table "public"."venue_statuses" drop constraint "venue_statuses_venue_id_fkey";
+
+alter table "public"."venue_statuses" drop constraint "venue_statuses_venue_status_state_id_fkey";
+
+alter table "public"."venue_statuses" drop constraint "venue_statuses_venue_status_type_id_fkey";
+
+alter table "public"."venues" drop constraint "venues_venue_types_id_fkey";
+
+alter table "public"."event_groups" drop constraint "event_groups_pkey";
+
+alter table "public"."event_types" drop constraint "event_types_pkey";
+
+alter table "public"."events" drop constraint "events_pkey";
+
+alter table "public"."user_roles" drop constraint "user_roles_pkey";
+
+alter table "public"."users" drop constraint "users_pkey";
+
+alter table "public"."venue_status_states" drop constraint "venue_status_states_pkey";
+
+alter table "public"."venue_status_types" drop constraint "venue_status_types_pkey";
+
+alter table "public"."venue_statuses" drop constraint "venue_statuses_pkey";
+
+alter table "public"."venue_types" drop constraint "venue_types_pkey";
+
+alter table "public"."venues" drop constraint "venues_pkey";
+
+drop index if exists "public"."event_groups_event_id_user_id_key";
+
+drop index if exists "public"."event_groups_pkey";
+
+drop index if exists "public"."event_types_pkey";
+
+drop index if exists "public"."events_pkey";
+
+drop index if exists "public"."user_roles_pkey";
+
+drop index if exists "public"."users_pkey";
+
+drop index if exists "public"."venue_status_states_pkey";
+
+drop index if exists "public"."venue_status_types_pkey";
+
+drop index if exists "public"."venue_statuses_pkey";
+
+drop index if exists "public"."venue_types_pkey";
+
+drop index if exists "public"."venues_pkey";
+
+drop table "public"."event_groups";
+
+drop table "public"."event_types";
+
+drop table "public"."events";
+
+drop table "public"."user_roles";
+
+drop table "public"."users";
+
+drop table "public"."venue_status_states";
+
+drop table "public"."venue_status_types";
+
+drop table "public"."venue_statuses";
+
+drop table "public"."venue_types";
+
+drop table "public"."venues";
+
+create table "public"."profiles" (
+    "id" uuid not null,
+    "role" text,
+    "first_name" text,
+    "last_name" text,
+    "phone_number" text,
+    "created_at" timestamp without time zone default now()
+);
+
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_role_check" CHECK ((role = ANY (ARRAY['cleaning'::text, 'manager'::text, 'reception'::text, 'repair'::text]))) not valid;
+
+alter table "public"."profiles" validate constraint "profiles_role_check";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.create_profile_on_signup()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into profiles (id, role)
+  values (new.id, 'reception'); -- Default role can be changed
+  return new;
+end;
+$function$
+;
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+
